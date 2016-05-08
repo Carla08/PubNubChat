62ed1a36fc6c56c1c3ecf1ac99b585a5fe59c879
Mario Contreras <mario.acc.15@gmail.com>
2016-05-07 19:54:37 -0600

    views, routes and public files added, .gitignore file added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+
+//var user ="mario";
+//var user ="dave";
+////var password="ithe2ichi7osel";
+//var password="dave";
+var domain="cml.chi.itesm.mx";
+//var port = 5222;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.render("index", {});
+
+
+  // logOff = function () {
+  //   req.app.locals.xmpp.disconnect();
+  // }
+  
+});
+
+router.post("/", function(req,res,next){
+  //res.cookie("jid",req.body.username + "@" + domain);
+  //req.app.locals.res=res;
+  //req.app.locals.req=req;
+  res.render('chat', {
+    jid: req.body.username + "@" + domain,
+    password: req.body.password});
+
+
+});
+
+router.get("/logoff" ,(req,res,next)=>{
+  //req.app.locals.xmpp.disconnect();
+  res.send("You are fucking out!");
+
 });
 
 module.exports = router;
